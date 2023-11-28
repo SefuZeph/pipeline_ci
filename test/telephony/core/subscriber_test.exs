be3963a25991d9192c2fcf9c47c6071beed2a942
Zeph <zephaniahsefuoduori@gmail.com>
2023-11-28 14:07:35 +0300

    Add prepaid and postpaid

--- test/telephony/core/subscriber_test.exs
+++ test/telephony/core/subscriber_test.exs
@@ -1,8 +1,8 @@
 defmodule Telephony.Core.SubscriberTest do
   use ExUnit.Case
-  alias Telephony.Core.Subscriber
+  alias Telephony.Core.{Subscriber,Prepaid,Postpaid}
 
-  test "create a subscriber" do
+  test "create a prepaid subscriber" do
     # given
     payload = %{full_name: "Gustavo", phone_number: "123", subscriber_type: :prepaid}
     # when
@@ -11,9 +11,24 @@ defmodule Telephony.Core.SubscriberTest do
     expect = %Subscriber{
       full_name: "Gustavo",
       phone_number: "123",
-      subscriber_type: :prepaid
+      subscriber_type: %Prepaid{credits: 0, recharges: []}
     }
 
     assert expect == result
   end
+
+  test "create a postpaid subscriber" do
+    # given
+    payload = %{full_name: "Gustavo", phone_number: "123", subscriber_type: :postpaid}
+    # when
+    result = Subscriber.new(payload)
+    # then
+    expect = %Subscriber{
+      full_name: "Gustavo",
+      phone_number: "123",
+      subscriber_type: %Postpaid{spent: 0}
+    }
+    assert expect == result
+  end
+
 end
